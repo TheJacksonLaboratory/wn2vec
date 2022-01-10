40ccef1e231cbff296310ad81c4cf4bd7cd31573
pnrobinson <peter.robinson@jax.org>
2022-01-10 15:39:00 +0000

    adding tests

--- wn2vec/wntransformer.py
+++ wn2vec/wntransformer.py
@@ -1,8 +1,33 @@
 
 #from nltk.corpus import wordnet as wn
 
+import os
+from collections import defaultdict
 
 
 class WordNetTransformer:
 
-    pass
+    def __init__(self, marea_file) -> None:
+        """
+        Path to the file produced by marea
+        """
+        if not os.path.exists(marea_file):
+            raise FileNotFoundError("Could not find marea file")
+
+        self._marea_file = marea_file
+        self._counter = defaultdict(int)
+        # Get count of words in corpus
+        with open(marea_file) as f:
+            for line in f:
+                words = line.split()
+                for w in words:
+                    # TODO skip stop words
+                    self._counter[w] += 1
+        # Create synonym dictionary with NLTK
+
+    def get_counter(self):
+        return self._counter
+            
+
+        
+            
