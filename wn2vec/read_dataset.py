24f6a87f703e2b10d0420031f719d6b1df798e8c
enockniyonkuru <enockniyonkuru250@gmail.com>
2022-01-13 10:13:47 -0500

    update

--- wn2vec/read_dataset.py
+++ wn2vec/read_dataset.py
@@ -1,9 +1,12 @@
 
+
 # Read ten first ten lines of the data set and prints them 
 from replace import*
 from collections import defaultdict
 import csv
-
+from nltk.corpus import wordnet as wn # Import Wordnet
+from collections import Counter  # Import Counter
+import numpy as np
 
 marea_file = "data/sample100abstracts.tsv"
 counter = defaultdict(int)
@@ -17,35 +20,6 @@ for row in read_tsv:
     break
 
 
-#Create a dictionary using all the data set
 
-tsv_file = open(marea_file)
-read_tsv = csv.reader(tsv_file, delimiter="\t")
-temp = []
-for row in read_tsv:
-  corpus_raw = row
-  raw_sentences = row[2].split('.')
-  sentences = []
-  for sentence in raw_sentences:
-    temp.extend(sentence.split())
-dictionary = dictCreate(arrangement(temp)) 
-tsv_file.close()
-
-#Replace the variables in data set with the synonyms (synset ID) from the dictionary
-
-tsv_file = open(marea_file)
-read_tsv = csv.reader(tsv_file, delimiter="\t")
-for row in read_tsv:
-  corpus_raw = row
-  raw_sentences = row[2].split('.')
-  sentences = []
-  for sentence in raw_sentences:
-    sentences.append(sentence.split())
-    for i in range(len(sentences)):
-      if sentences[0][i] in dictionary:
-        continue
-      else:
-        x = giveKey(sentences[i], dictionary)
-        sentences[i] = x
-  print(sentences[0])
-tsv_file.close()
+dictionary_from_list(marea_file)
+replace_dataSet(marea_file)
