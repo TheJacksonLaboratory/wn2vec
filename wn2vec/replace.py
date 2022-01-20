0459e30b1f6249ae492f319973697ae312f37531
Peter Robinson <peter.robinson@jax.org>
2022-01-20 09:49:49 -0500

    updates

--- wn2vec/replace.py
+++ wn2vec/replace.py
@@ -1,5 +1,7 @@
 import nltk
 import ssl
+from typing import List
+from collections import Counter
 # to disable SSL to be able to download worldnet
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -20,11 +22,20 @@ Arragnement(): takes a list and returns list of sorted unique variables accordin
             @return: 'unique' a list of sorted variables in order of frequency  
             i.e the first element is a unique word which as a high frequency
 """
-def arrangement(list):
+def sort_words_by_count(list: List[str]) -> List[str]:
     result = sorted(list, key = list.count, reverse = True) # sorting on basis of frequency of elements
     used = set()
     unique = [x for x in result if x not in used and (used.add(x) or True)]  #arrange according to unique characters
     return(unique)
+
+
+def sort_words_by_count2(list: List[str]) -> List[str]:
+    counter = Counter(list)
+    # sort words (keys) by their frequency in 'list' (value)
+    sorted_keys = [pair[0] for pair in sorted(counter.items(), key=lambda item: item[1], reverse=True)]
+    return sorted_keys
+
+
 
 """
 synonym(): Takes a word and prints its synonyms in form of a list (synset) using wordnet 
@@ -34,26 +45,36 @@ synonym(): Takes a word and prints its synonyms in form of a list (synset) using
 def synonym(word):
     synonyms = []
     for syn in wn.synsets(word):
-        for l in syn.lemmas():
-            synonyms.append(l.name())
+        #for l in syn.lemmas():
+        #    synonyms.append(l.name())
+        synonyms.extend([l.name() for l in syn.lemmas()])
     return synonyms
 
 """
-dictCreate(): Creates a dictionary from the whole data set, they keys are in order of their frequency words and the values are synonyms of keys form synset   
+dictCreate(): Creates a dictionary from the whole data set, 
+they keys are in order of their frequency words and the values 
+are synonyms of keys form synset   
         @argument: 'unique' a list of unique variables from the wholed dataset in order of their frequency
         @return: 'dictionary' a dictionary of all the variables in the dataset, the keys are the unique variables with high frequency, and values are key's synonym
 """
-def dictCreate(unique):
+def dictCreate(unique: List[str]):
     dictionary = {} 
-    i = 0
-    while i < len(unique):
-      if unique[i] in dictionary:
-        i = i+1
-      else:
-        for x in range(0, len(synonym(unique[i]))):
-          dictionary[synonym(unique[i])[x]] = unique[i]
-        i = i+1
-    return dictionary
+    synonym2keyword_d = {}
+    for word in unique:
+        #synonym2keyword_d[word] = word
+        for syn in synonym(word):
+            if syn in synonym2keyword_d : continue
+            synonym2keyword_d[syn] = word
+    return synonym2keyword_d
+   # i = 0
+    #while i < len(unique):
+     # if unique[i] in dictionary:
+      #  i = i+1
+      #else:
+      #  for x in range(0, len(synonym(unique[i]))):
+      #    dictionary[synonym(unique[i])[x]] = unique[i]
+      #  i = i+1
+    #return dictionary
 
 """
 giveValue(): Takes a word and dictionary and returns the value of the word in the dictionary  
