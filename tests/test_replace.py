6f3b58bec8b7e89a2e941329cde5657898c5f559
Enock Niyonkuru <73262017+enockniyonkuru@users.noreply.github.com>
2022-05-27 10:58:10 -0400

    updated test_replace.py and replace_data_set function
    updated test_replace.py and replace_data_set function

--- tests/test_replace.py
+++ tests/test_replace.py
@@ -1,5 +1,5 @@
 import unittest
-from script.replace import*
+from wn2vec.wntransformer import*
 
 import nltk
 
@@ -10,31 +10,36 @@ import numpy as np
 import csv
 
 class ReplaceTestCase(unittest.TestCase):
-    def test_sort_words_by_count(self):
-        test1 = ['cancer', 'disease', 'caused', 'cell', 'crab', 'simple', 'cadre', 'cell', 'cancer', 'disease', 'cell', 'cancer', 'cell']
-        result = Replace.sort_words_by_count(test1)
-        expected = ['cell', 'cancer', 'disease', 'caused', 'crab', 'simple', 'cadre']
-        self.assertEqual(expected, result, 'Arrangement do not match expected.')
-        # 'cancer', 'disease', 'caused', 'cell', 'crab', 'simple', 'cadre']
-
     def test_synonym(self):
-        expected = ['tissue', 'tissue', 'tissue_paper', 'weave', 'tissue']
-        result = Replace.synonym('tissue')
+        expected = ['colony', 'colony', 'settlement', 'colony', 'Colony', 'colony', 'colony', 'dependency', 'colony']
+        result = wntransformer.synonym('colony')
         self.assertEqual(expected, result, 'Synonym do not match expected.')
-    
+
+    def _highest_count_synonym(self):
+        test1 = ['inhibition', 'inhibition', 'suppression', 'inhibition', 'inhibition', 'prohibition', 'inhibition', 'forbiddance']
+        counter = {'evaluation': 2, 'dye': 2, 'exclusion': 2, 'colony': 8, 'settlement': 15, 'inhibition': 17,
+                   'prohibition': 50}
+        result = wntransformer._highest_count_synonym(test1)
+        expected = ['prohibition']
+        self.assertEqual(expected, result, 'Arrangement do not match expected.')
+
+
+
     def test_dictCreate(self):
-        test_list = ['cadre', 'tissue', 'cell', 'weave', 'cider']
-        expected_dictionary = {'cell': 'cadre', 'cadre': 'cadre', 'tissue': 'tissue', 'tissue_paper': 'tissue', 'weave': 'tissue', 'cider': 'cider', 'cyder': 'cider'}
-        rep = Replace(test_list)
-        d = rep.get_word_dictionary()
-        self.assertEqual(expected_dictionary, d, 'Dictionary created do not match expected.')
+        test_list = ['prohibition', 'inhibition', 'settlement', 'colony', 'exclusion', 'dye', 'evaluation']
+        counter = {'evaluation': 2, 'dye': 2, 'exclusion': 2, 'colony': 8, 'settlement': 15, 'inhibition': 17, 'prohibition': 50}
+        expected_dictionary = {'inhibition': 'prohibition', 'settlement': 'settlement', 'colony': 'settlement', 'exclusion': 'exclusion',
+                                        'dye': 'dye', 'evaluation': 'evaluation'}
+        results = wntransformer.dictCreate(test_list)
+        self.assertEqual(expected_dictionary, results, 'Dictionary created do not match expected.')
       
     
     def test_replace_data_set(self):
-        test_set = ['cell', 'cadre', 'cell', 'dyestuff', 'dye', 'dye']
-        replace = Replace(list=test_set)
-        result = replace.get_replaced_data()
-        expected_set =['cell', 'cell', 'cell', 'dye', 'dye', 'dye']
+        test_set = ['evaluation', 'add', 'exclusion', 'total', 'basis', 'base', 'indicates', 'polyoma']
+        dictionary = {'inhibition': 'prohibition', 'settlement': 'settlement', 'colony': 'settlement',
+                      'exclusion': 'exclusion', 'dye': 'dye', 'evaluation': 'evaluation'}
+        result = wntransformer.replace_data_set(test_set,dictionary)
+        expected_set = ['evaluation', 'total', 'exclusion', 'total', 'base', 'found', 'show', 'polyoma']
         self.assertEqual(expected_set, result, 'Key do not match expected.')
 
 if __name__ == '__main__':
