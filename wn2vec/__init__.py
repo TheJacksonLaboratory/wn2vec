46ddb0049715030b75140a93d65294f4e394da00
pnrobinson <peter.robinson@jax.org>
2022-08-18 14:53:24 -0400

    update

--- wn2vec/__init__.py
+++ wn2vec/__init__.py
@@ -1,8 +1,15 @@
-from re import I
+
 from .wntransformer import WordNetTransformer
 from .tf_concept_parser import TfConceptParser
 from .tf_concept import TfConcept
 from .tTest import Ttest
+from .concept_set import ConceptSet
+from .concept_set_parser import ConceptSetParser
 
 
-__all__ = ['TfConceptParser', 'WordNetTransformer', TfConcept, Ttest]
+__all__ = ['ConceptSet',
+           'ConceptSetParser',
+           'TfConceptParser',
+            'TfConcept',
+            'Ttest',
+           'WordNetTransformer']
