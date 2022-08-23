4f20501b8ae01899ed5956da36f93e1b7b3689b7
Enock Niyonkuru <73262017+enockniyonkuru@users.noreply.github.com>
2022-08-22 22:31:59 -0400

    updated mestransformer

--- meshSetTransformer.py
+++ meshSetTransformer.py
@@ -16,22 +16,32 @@ args = parser.parse_args()
 input_dir = args.i
 out_fname = args.o
 
-
-
 class MeSHSet:
-    def __init__(self, symbols):
+    def __init__(self, name, id, symbols):
+        self._name = name
+        self._id = id
         self._symbols = symbols
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def symbols(self):
         return self._symbols
 
 
-
 def process_MeSH_file(fname):
     print(f"Processing {fname}")
     if not os.path.isfile(fname):
         raise FileNotFoundError(f"Could not find {fname}")
+    mapped_symbols = None
+    standard_name = None
+    systematicName = None
 
     mesh_list = []
     with open(fname, 'r') as file:
@@ -39,10 +49,17 @@ def process_MeSH_file(fname):
         for row in csvreader:
             meshid = row[0].split('\t')[0].replace("D","meshd" ) # [0]: 'MAPPED_SYMBOLS', [1]: list of mesh & D000182 --> meshc000657245
             mesh_list.append(meshid)
-    return MeSHSet(symbols=mesh_list)
 
+    standard_name = 'meshset' # This is to hold the position to keep the same format as geneset
+    systematicName = 'D0000' # This is to hold the position to keep the same format as geneset
+    mapped_symbols = mesh_list
 
-mesh_list_of_list = []
+    if mapped_symbols is None or standard_name is None or systematicName is None:
+        raise ValueError(f"Could not parse MSigDb gene set properly: {fname}")
+
+    return MeSHSet(name=standard_name, id=systematicName, symbols=mapped_symbols)
+
+mesh_set_list = []
 
 dir_list = os.listdir(input_dir)
 
@@ -52,13 +69,15 @@ for f in dir_list:
 
     fullpath = os.path.join(input_dir, f)
     meshset = process_MeSH_file(fname=fullpath)
-    mesh_list_of_list.append(meshset)
+    mesh_set_list.append(meshset)
 
 fh = open(out_fname, 'wt')
 
-for meshset in mesh_list_of_list:
+for meshset in mesh_set_list:
+    name = meshset.name
+    id = meshset.id
     symbols = meshset.symbols
-    fh.write("%s\n" % (";".join(symbols)))
 
-fh.close(),
+    fh.write("%s\t%s\t%s\n" % (name, id, ";".join(symbols)))
+fh.close()
 
