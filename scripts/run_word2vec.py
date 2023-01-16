afa0980a16655591d49e9ef22ccc6fe7f073dac0
Peter Robinson <peter.robinson@jax.org>
2023-01-16 07:56:18 -0500

    updates

--- scripts/run_word2vec.py
+++ scripts/run_word2vec.py
@@ -1,16 +1,11 @@
-import io
-import re
-import string
-import tqdm
+
 import logging
 import datetime
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
+
 import argparse
 import time
 
-from tf_word2vecRunner import Word2VecRunner
+from wn2vec import Word2VecRunner
 
 
 today_date = datetime.date.today().strftime("%b_%d_%Y")
@@ -26,8 +21,9 @@ logging.basicConfig(level=logging.INFO, filename=logname, filemode='w', datefmt=
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, required=True, help ='address of the .tsv file with abstracts') 
 parser.add_argument('-v', type=str, default='vector', help='name of vector file output')
-parser.add_argument('-m', type=str, default='metadata', help='name of metadata file')
-parser.add_argument('vocab_size', type=int, default=5000)
+parser.add_argument('-m', '--metadata', type=str, default='metadata', help='name of metadata file')
+parser.add_argument('--embedding_dim', type=int, default=128)
+parser.add_argument('--vocab_size', type=int, default=50000)
 args = parser.parse_args()
 
 
@@ -36,10 +32,18 @@ input_file_path = args._i
 vector_name = args._v
 metadata_name = args._m
 vocab_size = args.vocab_size
+embed_dim = args.embedding_dim
 
-runner = Word2VecRunner( input = input_file_path, vector = vector_name, metadata = metadata_name, vocab_size = 5000, 
-                             embedding_dim= 128, sequence_length = 10, window_size = 2, 
-                                  BATCH_SIZE = 128, BUFFER_SIZE= 10000, num_ns = 4, SEED = 42 )
+runner = Word2VecRunner(input=input_file_path, vector=vector_name, metadata=metadata_name, vocab_size=5000, 
+                             embedding_dim=embed_dim, sequence_length = 10, window_size = 2, 
+                              BATCH_SIZE = 128, BUFFER_SIZE= 10000, num_ns = 4, SEED = 42 )
+"""
+TODO -- adapt script to 
+runner.input_file()
+runner.embed_file()
+runner.output_vectors(outputfile="???")
+
+"""
 
 
 
