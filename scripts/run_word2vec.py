6298e2caf79a511adafbf85e40288c1f41a1928d
Peter Robinson <peter.robinson@jax.org>
2023-01-20 08:59:09 -0500

    cleanup

--- scripts/run_word2vec.py
+++ scripts/run_word2vec.py
@@ -10,10 +10,7 @@ from tf_word2vecRunner import Word2VecRunner
 
 
 today_date = datetime.date.today().strftime("%b_%d_%Y")
-start_time = time.time()
-start_time_fmt= time.strftime("%H:%M:%S", time.gmtime(start_time))
-
-logname = f"wn2vec_{today_date} {start_time_fmt}.log"
+logname = f"wn2vec_{today_date}.log"
 
 logging.basicConfig(level=logging.INFO, filename=logname, filemode='w', datefmt='%Y-%m-%d %H:%M:%S', 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -23,6 +20,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, default='/Users/niyone/Desktop/word2vector/data/10ktest.tsv', help ='address of the .tsv file with abstracts') 
 parser.add_argument('-v', type=str, default='vector', help='name of vector file output')
 parser.add_argument('-m', type=str, default='metadata', help='name of metadata file')
+parser.add_argument('-w', '--window_size', type=int, default=2, help='size of embedding window')
+
 parser.add_argument('--embedding_dim', type=int, default=128)
 parser.add_argument('--vocab_size', type=int, default=5000)
 args = parser.parse_args()
@@ -34,11 +33,33 @@ vector_name = args.v
 metadata_name = args.m
 vocab_size = args.vocab_size
 embed_dim = args.embedding_dim
+window_size = args.window_size
 num_ns = 4
+sequence_length = 10
+BATCH_SIZE = 128
+BUFFER_SIZE= 10000
+SEED = 42
+
 
 runner = Word2VecRunner(input_file=input_file_path, vector=vector_name, metadata=metadata_name, vocab_size=5000, 
-                             embedding_dim=embed_dim, sequence_length = 10, window_size = 2, 
+                             embedding_dim=embed_dim, sequence_length = sequence_length, window_size = window_size, 
                               BATCH_SIZE = 128, BUFFER_SIZE= 10000, num_ns = 4, SEED = 42 )
+
+logging.info("starting run_word2vec.py")
+logging.info(f"input_file_path: {input_file_path}")
+logging.info(f"vector_name: {vector_name}")
+logging.info(f"metadata_name: {metadata_name}")
+logging.info(f"embed_dim: {embed_dim}")
+logging.info(f"sequence_length: {sequence_length}")
+logging.info(f"BATCH_SIZE: {BATCH_SIZE}")
+logging.info(f"BUFFER_SIZE: {BUFFER_SIZE}")
+logging.info(f"SEED: {SEED}")
+
+
+
+
+ 
+                              
 
 
 runner.input_file()
