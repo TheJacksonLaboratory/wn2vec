0f448b11b9690aa46394cb6480b263af320be211
Peter Robinson <peter.robinson@jax.org>
2023-02-12 12:16:45 -0500

    refactor wn transformer class

--- scripts/run_wn_replacement.py
+++ scripts/run_wn_replacement.py
@@ -6,32 +6,26 @@ import numpy as np
 sys.path.insert(0, os.path.abspath('../src/'))
 from wn2vec import WordNetTransformer
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', type=str, required=True, help ='path to filtered marea_file')
-parser.add_argument('-o', type=str,  required=True, help ='path to of output_file') 
-parser.add_argument('--threshold', type=float, help = "threshold (0-1, default median word threshold)")
+parser.add_argument('-i', type=str, required=True, help='path to filtered marea_file')
+parser.add_argument('-o', type=str, required=True, help='path to of output_file')
+parser.add_argument('--threshold', type=float, help="threshold (0-1, default median word threshold)")
 args = parser.parse_args()
 
-marea_input_file = args.i 
+marea_input_file = args.i
 output_file = args.o
 threshold = args.threshold
 if not os.path.isfile(marea_input_file):
     raise FileNotFoundError(f"Could not find marea [intput] file at {marea_input_file}")
 
-
-
-
-
-
 transformer = WordNetTransformer(marea_file=marea_input_file, percentile=threshold)
 threshold = transformer.get_threshold()
-print(f'Threshold: {threshold} at percentile {100*threshold}')
+print(f'Threshold: {threshold} at percentile {100 * threshold}')
 replaced_words = transformer.get_replaced_word_count()
 print("Number of replaced words: ", replaced_words)
 transformer.transform_and_write(output_file=output_file)
 
-# sample way of running the code using arparse:
+
 """
 example: 
 > python ../scripts/run_wn_replacement.py -i ../data/pubmed_filt/100pubmed_filt.tsv -o ../data/pubmed_wn/100pubmed_wn.tsv --threshold 0.3
