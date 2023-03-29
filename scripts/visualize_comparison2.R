6061801180f52ad501e0a29e0bcaf117f4f709a2
pnrobinson <peter.robinson@jax.org>
2023-03-29 17:58:05 -0400

    visualize

--- scripts/visualize_comparison2.R
+++ scripts/visualize_comparison2.R
@@ -1,11 +1,12 @@
 
 library("ggplot2")
+library("ggsci")
  
 # Considering P-Value for significancy
 sets_counts_per_concept_set <- c(10, 0, 54, 9, 36, 4, 25, 10, 27, 6)
 
-concept_set_name <- rep(c( 'Mesh Sets','biocarta', 'kegg_canonical', 
-                                   'bp_gene_ontology', 'pid_canonical'),each = 2)
+concept_set_name <- rep(c( 'MeSH','Biocarta', 'KEGG', 
+                                   'GO (bp)', 'PID'),each = 2)
 
 
 subgroup <- rep(c("Wordnet" , "Pubtator" ))
@@ -16,7 +17,11 @@ ggplot(data, aes(x = concept_set_name,
            y = sets_counts_per_concept_set,
            fill = subgroup)) +
   geom_bar(stat = "identity",
-           position = "dodge")
+           position = "dodge") +
+            theme(axis.title.x = element_blank(),
+                    axis.title.y = element_blank(),
+                    axis.text = element_text(size = 14)) +
+        scale_fill_npg()
 
 
 ggsave('P_mean_significant.pdf')
@@ -39,8 +44,10 @@ data <- data.frame(sets_counts_per_concept_set, concept_set_name, subgroup)
 ggplot(data, aes(x = concept_set_name,
            y = sets_counts_per_concept_set,
            fill = subgroup)) +
-  geom_bar(stat = "identity",
-           position = "dodge")
+            geom_bar(stat = "identity", position = "dodge") +
+            theme(axis.title.x = element_blank(),
+                    axis.title.y = element_blank(),
+                    axis.text = element_text(size = 14, face = "bold"))
 
 
 ggsave('Regardless_P_mean_significant.pdf')
