f05b07ee89324571f21948f27d7b1f7a58f7dc11
Enock Niyonkuru <73262017+enockniyonkuru@users.noreply.github.com>
2023-04-04 01:09:25 -0400

    update vis

--- scripts/visualize_comparison.R
+++ scripts/visualize_comparison.R
@@ -1,62 +1,50 @@
-library(ggplot2)
-library(dplyr)
-library(plyr)
-library(tidyr)
-library(RColorBrewer)
+library("ggplot2")
+library("ggsci")
 
-# Input the output file from the compare_embeddings.py script
-dat <- read.table("comn_concepts.tsv", sep="\t", header=T)
-
-# First do binomial test for random chance =0.5
-
-k <- sum(dat$mean_distance_pm > dat$mean_distance_wn)
-n <- dim(dat)[1]
-binom.test(k, n)
-
-## Paired t test
-t.test(dat$mean_distance_wn, dat$mean_distance_pm, paired=TRUE, alternative="less")
-
-dat <- dat %>% pivot_longer(cols=c('mean_distance_pm', 'mean_distance_wn'),names_to = "embedding", 
-    values_to ="distance")
-dat <- dat[c("embedding", "distance")]
-dat$embedding[dat$embedding == 'mean_distance_pm'] <- 'Pubtator'
-dat$embedding[dat$embedding == 'mean_distance_wn'] <- 'Wordnet'
-
-# to calculate sd etc
-data_summary <- function(data, varname, groupnames){
-  #require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
-  }
-  data_sum<-ddply(data, groupnames, .fun=summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
- return(data_sum)
+if(!require('ggpubr')) {
+  install.packages('ggpubr')
+  library('ggpubr')
 }
 
-df3 <- data_summary(dat, varname="distance", 
-                    groupnames=c("embedding"))
-# Convert dose to a factor variable
-df3$dose=as.factor(df3$embedding)
-head(df3)
+# Considering P-Value for significancy
+sets_counts_per_concept_set <- c(10, 0, 54, 9, 36, 4, 25, 10, 27, 6)
 
-p <- ggplot(df3, aes(x=embedding, y=distance, fill=embedding)) + 
-   geom_bar(stat="identity", position=position_dodge(), width=0.6) +
-  geom_errorbar(aes(ymin=distance-sd, ymax=distance+sd), width=.05, position=position_dodge(.9)) +
-  theme_minimal() +
-  scale_fill_brewer(palette = "Dark2") +
-  theme(axis.text=element_text(size=24),
-        axis.title=element_blank(),
-        legend.position = "none")
-ggsave('pt_vs_wn.pdf')
-    
-  
-## DO stats
-t.test(dat$distance[dat$embedding=="Pubtator"], dat$distance[dat$embedding=="Wordnet"])
+concept_set_name <- rep(c('MeSH', 'Biocarta', 'KEGG', 'GO (bp)', 'PID'), each = 2)
+sets_counts_per_concept_set2 <- c(57, 24, 178, 104, 120, 53, 133, 93, 124, 70)
+
+subgroup <- rep(c("Wordnet", "Pubtator"))
+
+data <- data.frame(sets_counts_per_concept_set, concept_set_name, subgroup)
 
 
-dat$embedding[dat$embedding == 'mean_distance_pm'] <- 'Pubtator'
-dat$embedding[dat$embedding == 'mean_distance_wn'] <- 'Wordnet'
+p1 <- ggplot(data, aes(x = concept_set_name,
+           y = sets_counts_per_concept_set,
+           fill = subgroup)) +
+            geom_bar(stat = "identity", position = "dodge") +
+            scale_fill_nejm() + theme_bw() +
+            theme(axis.title.x = element_blank(), 
+                  axis.title.y = element_blank(), 
+                  axis.text = element_text(size = 15)) +
+            labs(color = NULL) +
+            guides(fill = guide_legend(title = NULL)) +
+            ylim(0, max(sets_counts_per_concept_set, sets_counts_per_concept_set2))
+
+#Rregardless of P-Value for Significancy 
+
+sets_counts_per_concept_set2 <- c(57, 24, 178, 104, 120, 53, 133, 93, 124, 70)
+
+data2 <- data.frame(sets_counts_per_concept_set2, concept_set_name, subgroup)
+
+p2 <- ggplot(data2, aes(x = concept_set_name,
+           y = sets_counts_per_concept_set2,
+           fill = subgroup)) +
+            geom_bar(stat = "identity", position = "dodge") +
+            scale_fill_nejm() + theme_bw() +
+            theme(axis.title.x = element_blank(), 
+                  axis.title.y = element_blank(), 
+                  axis.text = element_text(size = 15)) +
+            guides(fill = guide_legend(title = NULL)) +
+            ylim(0, max(sets_counts_per_concept_set, sets_counts_per_concept_set2))
 
 
+print(ggarrange(p1, p2, common.legend = TRUE))
